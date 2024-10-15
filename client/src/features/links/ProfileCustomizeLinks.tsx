e8671ba12f721ab327f818b6b89c9a3bfd4c4c7f
Elias Soykat <eliasmd624@gmail.com>
2024-10-15 09:09:42 +0600

    fix : add github dummy user link when link craeted

--- client/src/features/links/ProfileCustomizeLinks.tsx
+++ client/src/features/links/ProfileCustomizeLinks.tsx
@@ -9,6 +9,7 @@ export default function ProfileCustomizeLinks({
   isCreating: boolean;
 }) {
   const { links, addLink } = useLinks();
+
   return (
     <div className="flex flex-col bg-white px-14 py-12 pb-0 mobile:p-[2.4rem] mobile:pb-0">
       <h1 className="block pb-[0.8rem] text-[3.1rem] font-bold leading-[4.8rem] text-[#333] tablet:hidden mobile:text-[2.4rem] mobile:leading-[3.5rem]">
@@ -20,7 +21,7 @@ export default function ProfileCustomizeLinks({
       </h3>
       <button
         className="mb-[2.4rem] flex items-center justify-center gap-[0.8rem] rounded-[0.8rem] border border-solid border-[#633cff] px-11 py-4 text-[1.6rem] font-medium leading-[2.4rem] text-[#633cff] hover:bg-[#efebff] disabled:cursor-not-allowed disabled:border-[#ccc] disabled:bg-[#ccc] disabled:text-[#666]"
-        onClick={() => addLink("Github", "", Date.now())}
+        onClick={() => addLink("Github", "https://github.com/users", Date.now())}
         disabled={links.length === 5}
       >
         <HiOutlinePlus />
